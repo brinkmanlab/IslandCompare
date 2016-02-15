ba39530784d960bf18f3118a8f8db2a205721d9b
adrianclim <aclim@sfu.ca>
2016-02-15 10:43:35 -0800

    Add method to parse sigihmm gff file

--- IslandCompare/genomeManage/libs/sigihmmwrapper.py
+++ IslandCompare/genomeManage/libs/sigihmmwrapper.py
@@ -26,6 +26,36 @@ def runSigiHMM(emblinput,embloutput,gffoutput):
     scriptFile.close()
     return None
 
-def test1():
+def parseSigiGFF(gffoutput):
+    # gffoutput = output from running SigiHMM
+    # returns a list of dicts of start, stop, and strand (+ or -)  of Genomic Islands
+    # Putal genes are considered Islands
+    listPutalGenes = []
+    with open(gffoutput,'r') as gff:
+        for line in gff:
+            if line[0] == '#':
+                continue
+            else:
+                cleanedLine = ' '.join(line.split())
+                geneDict = cleanedLine.split(' ')
+                if geneDict[2] == 'PUTAL':
+                    try:
+                        details = geneDict[8]
+                    except:
+                        details = ''
+                    listPutalGenes.append({'start':geneDict[3],'end':geneDict[4],
+                                           'strand':geneDict[6],'details':details})
+                else:
+                    pass
+    return listPutalGenes
+
+def testRunSigiHMM():
     testfiledir = os.path.dirname(os.path.realpath(__file__))+"/testfiles/"
     runSigiHMM(testfiledir+"Pseudomonas_aeruginosa_PAO1_107.embl","/tmp/testsigi.embl","/tmp/testsigi.gff")
+
+def testParser():
+    testfile = "/data/sigi/AE009952.1.gff"
+    output = parseSigiGFF(testfile)
+    for line in output:
+        print output
+        print '\n'
