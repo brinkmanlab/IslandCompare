f0b0c9149554665ab75b960176d42d1e1c801223
adrianclim <aclim@sfu.ca>
2016-02-26 11:48:11 -0800

    Updated UI of dropzone, added check for correct file type client side.

--- IslandCompare/genomeManage/email.py
+++ IslandCompare/genomeManage/email.py
@@ -4,7 +4,7 @@ from django.conf import settings
 
 def sendAnalysisCompleteEmail(userEmail, jobId):
     if settings.SEND_EMAIL:
-        send_mail('IslandCompare - Job Complete', 'Your Job has completed',
+        send_mail('IslandCompare - Job Complete', 'Your Job has Completed',
                   settings.EMAIL_SENDER, [userEmail], fail_silently=False)
 
     logging.info("Sent Email To : "+userEmail)
