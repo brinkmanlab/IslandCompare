85421d6d13c6c6228f52cd12e91f95178ca0ea4b
adrianclim <aclim@sfu.ca>
2016-03-02 10:19:18 -0800

    Add method to parse mauve backbone on server

--- IslandCompare/genomeManage/libs/mauvewrapper.py
+++ IslandCompare/genomeManage/libs/mauvewrapper.py
@@ -2,6 +2,7 @@ from tempfile import NamedTemporaryFile
 import subprocess
 import os
 import shutil
+import csv
 
 MAUVE_PATH = "/apps/mauve_snapshot_2015-02-13/linux-x64/progressiveMauve"
 MAUVE_OUTPUT_PATH = "/data/mauve"
@@ -32,9 +33,27 @@ def runMauve(sequencepaths, outputbackbonepath):
     scriptFile.close()
     return None
 
+def parseMauveBackbone(backbonePath):
+    # input path to backbone file
+    # mauveOutput will look like:
+    # [{seq1:[start,end],seq2:[start,end]},{seq1:[start,end],seq2:[start,end]}]
+    mauveOutput = []
+    with open(backbonePath,'r') as backbone:
+        tsvReader = csv.reader(backbone, delimiter='\t')
+        header = tsvReader.next()
+        for row in tsvReader:
+            homologousRegion ={}
+            for sequence in range(0,len(row)/2):
+                homologousRegion[sequence]=row[sequence*2],row[sequence*2+1]
+            mauveOutput.append(homologousRegion)
+    return mauveOutput
+
 ### Tests
 
 def test1():
     testfiledir = os.path.dirname(os.path.realpath(__file__))+"/testfiles/"
     seqpaths = [testfiledir+"AE009952.gbk",testfiledir+"BX936398.gbk"]
     runMauve(seqpaths, "/tmp/test1")
+
+def test2():
+    print parseMauveBackbone("/data/mauve/3-4-5-6-7-8-9.backbone")
