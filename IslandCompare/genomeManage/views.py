4db269f7de1955e1bec4b06310d9081d6f44468b
adrianclim <aclim@sfu.ca>
2016-03-21 15:20:07 -0700

    Add updatable genome names

--- IslandCompare/genomeManage/views.py
+++ IslandCompare/genomeManage/views.py
@@ -64,11 +64,11 @@ def uploadGenome(request):
     downloadedFiles = request.FILES.getlist('uploadedGenomes')
     for uploadedfile in downloadedFiles:
         if uploadedfile.name.endswith('.gbk') or uploadedfile.name.endswith('.gb'):
-            genome = Genome(uploadedName=uploadedfile.name,uploader=request.user,genbank=uploadedfile)
+            genome = Genome(givenName=uploadedfile.name, uploadedName=uploadedfile.name,uploader=request.user,genbank=uploadedfile)
             genome.save()
             parseGenbankFile(genome.id)
         elif uploadedfile.name.endswith('.embl'):
-            genome = Genome(uploadedName=uploadedfile.name,uploader=request.user,embl=uploadedfile)
+            genome = Genome(givenName=uploadedfile.name, uploadedName=uploadedfile.name,uploader=request.user,embl=uploadedfile)
             genome.save()
     return index(request)
 
@@ -138,6 +138,7 @@ def getGenomes(request):
     for genome in genomes:
         currentGenome = []
         currentGenome.append(genome.id)
+        currentGenome.append(genome.givenName)
         currentGenome.append(genome.name)
         currentGenome.append(genome.length)
         currentGenome.append(genome.description)
@@ -146,6 +147,24 @@ def getGenomes(request):
     tableData['data']=outputArray
 
     return JsonResponse(tableData, safe=False)
+
+@login_required(login_url='/login')
+@require_http_methods(["POST"])
+def updateGenome(request):
+    # Updates a genomes givenName
+    success = False
+    try:
+        genomeId = request.POST.get("id")
+        newName = request.POST.get("name")
+
+        targetGenome = Genome.objects.get(id=genomeId)
+        targetGenome.givenName = newName
+        targetGenome.save()
+        success = True
+    except:
+        raise Exception("Updating Genome Name Failed")
+    finally:
+        return JsonResponse({"success":success})
 
 @login_required(login_url='/login')
 def getJobs(request):
