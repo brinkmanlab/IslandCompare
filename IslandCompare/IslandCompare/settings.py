b4a22ea4ccd7beaf5a8f375e465b4504d54194b2
adrianclim <aclim@sfu.ca>
2016-03-24 14:50:28 -0700

    Let celery serialize messages in JSON instead of pickle (security issue)

--- IslandCompare/IslandCompare/settings.py
+++ IslandCompare/IslandCompare/settings.py
@@ -140,3 +140,6 @@ EMAIL_SENDER = ''
 SEND_EMAIL = False
 
 CELERY_RESULT_BACKEND='djcelery.backends.database:DatabaseBackend'
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_ACCEPT_CONTENT = ['json']
