eba2c752a722701b159833a4349ff41c52b480d6
BeefTron <beeftilley@gmail.com>
2017-06-22 18:00:36 -0700

    SigiHMM and islandpath failures do not crash pipeline, merge_gis and mash will get skipped. Status reflected in job history

--- IslandCompare/analysis/tasks.py
+++ IslandCompare/analysis/tasks.py
@@ -1,5 +1,5 @@
 from __future__ import absolute_import, unicode_literals
-from celery import shared_task
+from celery import shared_task, chain
 from analysis.models import AnalysisComponent
 from analysis import components
 from analysis.pipeline import PipelineSerializer
@@ -75,26 +75,18 @@ def run_pipeline(self, serialized_pipeline):
     pipeline.analysis.celery_task_id = self.request.id
     pipeline.analysis.save()
 
-    index = len(pipeline.pipeline_components) - 1
-    next_component = None
-
-    while index != 0:
-        if next_component is None:
-            next_component = run_pipeline_component.s(pipeline.analysis.id,
-                                                      pipeline.pipeline_components[index].name,
-                                                      pipeline.pipeline_components[index].param)
+    component_list = serialized_pipeline['pipeline_components']
+    tasks = [run_pipeline_component.s(serialized_pipeline, pipeline.analysis.id, "start_pipeline")]
+    for comp in component_list[1:]:
+        if comp == "sigi":
+            tasks.append(run_pipeline_component.s(pipeline.analysis.id, comp)
+                         .on_error(sigi_error_handler.s(pipeline.analysis.id)))
+        elif comp == "islandpath":
+            tasks.append(run_pipeline_component.s(pipeline.analysis.id, comp)
+                         .on_error(ipath_error_handler.s(pipeline.analysis.id)))
         else:
-            current_component = run_pipeline_component.s(pipeline.analysis.id,
-                                                         pipeline.pipeline_components[index].name,
-                                                         pipeline.pipeline_components[index].param)
-            current_component.link(next_component)
-            next_component = current_component
-        index -= 1
-
-    task_chain = run_pipeline_component.s(serialized_pipeline,
-                                          pipeline.analysis.id,
-                                          pipeline.pipeline_components[index].name)
-    task_chain.link(next_component)
+            tasks.append(run_pipeline_component.s(pipeline.analysis.id, comp))
+    task_chain = chain(*tasks)
 
     logger.info("End scheduling of pipeline components with celery task id: {}".format(self.request.id))
     return task_chain.apply_async()
@@ -103,7 +95,7 @@ def run_pipeline(self, serialized_pipeline):
 @shared_task(bind=True)
 def run_pipeline_component(self, report, analysis_id, pipeline_component_name, pipeline_components_param=None):
     """
-    Runs a pipleine component
+    Runs a pipeline component
     :param self:
     :param report:
     :param analysis_id:
@@ -112,12 +104,24 @@ def run_pipeline_component(self, report, analysis_id, pipeline_component_name, p
     :return:
     """
     pipeline_component = PipelineComponentFactory.create_component(pipeline_component_name, pipeline_components_param)
-    logger.info("Attempting to run component: {} for analysis with id: {}".format(pipeline_component_name,
-                                                                                   analysis_id))
+    logger.info("Attempting to run component: {} for analysis with id: {}".format(pipeline_component_name, analysis_id))
 
-    analysis_component = AnalysisComponent.objects.get(analysis__id=analysis_id,
-                                                       type__name=pipeline_component.name)
+    analysis_component = AnalysisComponent.objects.get(analysis__id=analysis_id, type__name=pipeline_component.name)
     analysis_component.celery_task_id = self.request.id
     analysis_component.save()
 
     return pipeline_component.run(report)
+
+
+@shared_task()
+def sigi_error_handler(context, exc, traceback, pipeline_id):
+    logger.info("Sigi HMM Failed! Merge_gis and mash_mcl will be skipped. analysis id: {}".format(pipeline_id))
+    task_chain = chain(run_pipeline_component.s(context.args[0], pipeline_id, "islandpath")
+                           .on_error(ipath_error_handler.s(pipeline_id)),
+                       run_pipeline_component.s(pipeline_id, "end_pipeline"))
+    return task_chain.apply_async()
+
+@shared_task()
+def ipath_error_handler(context, exc, traceback, pipeline_id):
+    logger.info("Islandpath Failed! Merge_gis and mash_mcl will be skipped. analysis id: {}".format(pipeline_id))
+    return run_pipeline_component.s(context.args[0], pipeline_id, "end_pipeline").apply_async()
