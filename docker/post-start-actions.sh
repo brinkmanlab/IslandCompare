bc298f817c56c3235330627f3e134d05824cff5b
Nolan Woods <nolan_w@sfu.ca>
2019-04-18 13:58:32 -0700

    Swap out post start for autoinstall

--- docker/post-start-actions.sh
+++
@@ -1,7 +0,0 @@
-# Install shed tools
-galaxy-wait -v
-shed-tools install -a admin -t /home/galaxy/workflows/IslandCompare.tools.yml
-#TODO && workflow-install -a admin -w /home/galaxy/workflows/IslandCompare.ga
-
-# Patch shed tools
-wget https://raw.githubusercontent.com/bgruening/galaxytools/bc9dc2fc8a806b312d679466d1ea581f3ded35ee/tools/text_processing/text_processing/awk.xml -o /shed_tools/toolshed.g2.bx.psu.edu/repos/bgruening/text_processing/a6f147a050a2/text_processing/awk.xml
