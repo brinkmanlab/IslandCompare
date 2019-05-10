0dd3804ebe693b7d8cb67ae7628d651a4625c34d
Nolan Woods <nolan_w@sfu.ca>
2019-05-10 13:07:17 -0700

    Add subworkflows and update autoinstall script

--- docker/post-start-actions.sh
+++ docker/post-start-actions.sh
@@ -1,8 +1,16 @@
 # Docs: https://ephemeris.readthedocs.io/en/latest/commands.html
-# Install shed tools
 galaxy-wait -v
-shed-tools install -v -g "http://localhost:80" -a admin -t /home/galaxy/workflows/IslandCompare.tools.yml
-workflow-install -v -g "http://localhost:80" -a admin --publish_workflows -w /home/galaxy/workflows/IslandCompare.ga
+for file in $LOCAL_FILES/workflows/*.ga
+do
+    # Generate tool requirements
+    workflow-to-tools -w "$file" -o "$file.yml" -l "IslandCompare"
+    
+    # Install shed tools
+    shed-tools install -v -g "http://localhost:80" -a admin -t "$file.yml"
+    
+    # Install workflow
+    workflow-install -v -g "http://localhost:80" -a admin --publish_workflows -w "$file"
+done
 
 # Patch shed tools
 wget https://raw.githubusercontent.com/bgruening/galaxytools/bc9dc2fc8a806b312d679466d1ea581f3ded35ee/tools/text_processing/text_processing/awk.xml -O /galaxy-central/database/shed_tools/toolshed.g2.bx.psu.edu/repos/bgruening/text_processing/a6f147a050a2/text_processing/awk.xml
