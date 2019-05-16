4e6983d8ea04e6fc007017441b5f15cf1ac47cc8
Nolan Woods <nolan_w@sfu.ca>
2019-05-16 14:00:50 -0700

    Add patched to deployment and comments

--- docker/post-start-actions.sh
+++ docker/post-start-actions.sh
@@ -1,3 +1,4 @@
+# This must be ran AFTER tool dependencies are fully installed via admin panel or future ephemeris tool-deps install tool
 # Docs: https://ephemeris.readthedocs.io/en/latest/commands.html
 galaxy-wait -v
 for file in $LOCAL_FILES/workflows/*.ga
@@ -13,9 +14,9 @@ do
 done
 
 # Patch shed tools
-wget https://raw.githubusercontent.com/bgruening/galaxytools/bc9dc2fc8a806b312d679466d1ea581f3ded35ee/tools/text_processing/text_processing/awk.xml -O /galaxy-central/database/shed_tools/toolshed.g2.bx.psu.edu/repos/bgruening/text_processing/a6f147a050a2/text_processing/awk.xml
+curl -L -s https://raw.githubusercontent.com/bgruening/galaxytools/bc9dc2fc8a806b312d679466d1ea581f3ded35ee/tools/text_processing/text_processing/awk.xml -o /galaxy-central/database/shed_tools/toolshed.g2.bx.psu.edu/repos/bgruening/text_processing/a6f147a050a2/text_processing/awk.xml
 
 # Patch tool_deps
-wget https://raw.githubusercontent.com/bioconda/bioconda-recipes/1bc6236d5260f55c8f76302b11e044f49bebf73d/recipes/mauve/MauveCM -O $GALAXY_CONDA_PREFIX/envs/__mauve@_uv_/share/mauve-2.4.0.r4736-0/MauveCM
-chmod +x $GALAXY_CONDA_PREFIX/envs/__mauve@_uv_/share/mauve-2.4.0.r4736-0/MauveCM
-ln -s $GALAXY_CONDA_PREFIX/envs/__mauve@_uv_/share/mauve-2.4.0.r4736-0/MauveCM $GALAXY_CONDA_PREFIX/envs/__mauve@_uv_/bin/MauveCM
+curl -L -s https://raw.githubusercontent.com/bioconda/bioconda-recipes/1bc6236d5260f55c8f76302b11e044f49bebf73d/recipes/mauve/MauveCM -o $GALAXY_CONDA_PREFIX/envs/__mauve@_uv_/share/mauve-2.4.0.r4736-0/MauveCM \
+&& chmod +x $GALAXY_CONDA_PREFIX/envs/__mauve@_uv_/share/mauve-2.4.0.r4736-0/MauveCM \
+&& ln -s $GALAXY_CONDA_PREFIX/envs/__mauve@_uv_/share/mauve-2.4.0.r4736-0/MauveCM $GALAXY_CONDA_PREFIX/envs/__mauve@_uv_/bin/MauveCM
