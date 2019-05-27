cfef27d3f1fa7203b26104b33967e58b3139b389
Nolan Woods <nolan_w@sfu.ca>
2019-05-27 16:43:28 -0700

    Add deflist markdownit plugin. Change to different gulp markdown task.

--- ui/gulpfile.js
+++ ui/gulpfile.js
@@ -1,15 +1,17 @@
 const { src, dest } = require('gulp');
-const md = require('gulp-markdown-it');
+const md = require('gulp-markdownit');
 const rename = require("gulp-rename");
+
+const MD_PLUGINS = ['markdown-it-deflist'];
 
 function defaultTask() {
     // Render static markdown to html fragments
-    return src('*.md')
-        .pipe(md(/*{options: {html: true, linkify: true}}*/))
+    return src('static/*.md')
+        .pipe(md({/*options: {html: true, linkify: true}*/ plugins: MD_PLUGINS}))
         .pipe(rename({
             extname: ".htm",
         }))
-        .pipe(dest('../public/'));
+        .pipe(dest('public/'));
 }
 
 exports.default = defaultTask
