f4be9a68d785777a07d9ced2c2f982043bd773e7
Nolan Woods <nolan_w@sfu.ca>
2019-06-03 08:46:33 -0700

    Add citation footer

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -2,10 +2,15 @@
     <div id="app">
         <Navigation></Navigation>
         <router-view class="content"/>
+        <footer>
+            <p>If you use IslandCompare please cite (a publication is underway, in the meantime please cite IslandViewer):</p>
+            <p>Bertelli, C. et al. "IslandViewer 4: Expanded prediction of genomic islands for larger-scale datasets" Nucleic Acids Research. 2017 May 2.
+                doi: <b-link href="https://academic.oup.com/nar/article-lookup/doi/10.1093/nar/gkx343">10.1093/nar/gkx343</b-link></p>
+        </footer>
         <b-navbar fixed="bottom" variant="primary" type="dark" size="sm" class="tosfooter">
             <b-nav-text class="mx-auto tosfooter_content">
-                By using this service you agree to the <b-link to="/terms">terms of service</b-link>.
                 For any publications resulting in the use of our services see <b-link to="/publications">how to cite this service</b-link>.
+                By using this service you agree to the <b-link to="/terms">terms of service</b-link>.
             </b-nav-text>
         </b-navbar>
     </div>
