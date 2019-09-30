26cd3b64f7152d16e01833312db7307f7310689f
Nolan Woods <nolan_w@sfu.ca>
2019-09-30 09:59:40 -0700

    Generalise post start actions

--- deployment/post-start-actions.sh
+++ deployment/post-start-actions.sh
@@ -1,15 +1,20 @@
 # This must be ran AFTER tool dependencies are fully installed via admin panel or future ephemeris tool-deps install tool
 # Docs: https://ephemeris.readthedocs.io/en/latest/commands.html
-galaxy-wait -v
-python -m ephemeris.install_tool_deps -v -g "http://localhost:80" -a admin -t $LOCAL_FILES/local_tools/tool_conf.xml
-for file in $LOCAL_FILES/workflows/*.ga
+SERVER="${SERVER:-http://localhost:80}"
+KEY="${KEY:-admin}"
+WORKFLOWS="${WORKFLOWS:-$LOCAL_FILES/workflows}"
+TOOLCONF="${TOOLCONF:-$LOCAL_FILES/local_tools/tool_conf.xml}"
+
+galaxy-wait -v -g "$SERVER"
+python -m ephemeris.install_tool_deps -v -g "$SERVER" -a $KEY -t "$TOOLCONF"
+for file in $WORKFLOWS/*.ga
 do
     # Generate tool requirements
     workflow-to-tools -w "$file" -o "$file.yml" -l "IslandCompare"
     
     # Install shed tools
-    shed-tools install -v -g "http://localhost:80" -a admin -t "$file.yml"
+    shed-tools install -v -g "$SERVER" -a $KEY -t "$file.yml"
     
     # Install workflow
-    workflow-install -v -g "http://localhost:80" -a admin --publish_workflows -w "$file"
+    workflow-install -v -g "$SERVER" -a $KEY --publish_workflows -w "$file"
 done
