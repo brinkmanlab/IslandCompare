5fa0ea98e357da0215a8eaa70cba87a7df9de4ce
Nolan Woods <nolan_w@sfu.ca>
2019-10-08 09:41:59 -0700

    Documentation and minor code cleanup

--- ui/src/app.config.js
+++ ui/src/app.config.js
@@ -4,9 +4,9 @@ Configuration options specific to the app
 
 export const workflow_name = "IslandCompare unpacked";
 export const base_path = '/';
-//export const galaxy_path = '/galaxy'; // TODO enable CORS for galaxy and change this to domain name
+//export const galaxy_path = '/galaxy';
 //export const galaxy_path = `${window.location.protocol}//${window.location.hostname}:81`;
-export const galaxy_path = 'http://ec2-13-57-236-209.us-west-1.compute.amazonaws.com:81';
+export const galaxy_path = `${window.location.protocol}//ec2-13-57-236-209.us-west-1.compute.amazonaws.com:81`;
 
 if (process.env.NODE_ENV === 'production') {
     // Production only
--- ui/src/auth.js
+++ ui/src/auth.js
@@ -7,14 +7,22 @@ import uuidgen from 'uuid/v1';
 import { galaxy_load } from "@/store";
 import { User } from "@/galaxy/src/api/users";
 
-export let gidPromise = null;
+export let gidPromise = null; // Only set uuid globally once per page load
 
+/**
+ * Attempt to recover uuid from url or browser cache
+ * @returns {string} UUID of user
+ */
 export function getUUID() {
     let id = (new URLSearchParams(location.search)).get('uuid') || localStorage.getItem('galaxysession_user_uuid');
     if (id && gidPromise === null) gidPromise = setGlobalID(id);
     return id;
 }
 
+/**
+ * Generate a UUID for the user if it does not already exist
+ * @returns {Promise<string>} UUID of user
+ */
 export async function getOrCreateUUID() {
     let id = getUUID();
     if (!id) {
@@ -31,6 +39,11 @@ export async function getOrCreateUUID() {
     return id;
 }
 
+/**
+ * Helper to register the UUID for the user throughout the app
+ * @param id {string} User UUID
+ * @returns {Promise<string>} Forwards the passed in UUID value
+ */
 export async function setGlobalID(id) {
     // register filter to append ?uuid= to urls
     Vue.filter('auth', value=>value + (value.includes('?') ? '&' : '?') + 'key=' + id);
