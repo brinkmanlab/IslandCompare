5c4a00afaecfe8edbf59c7137d8af5dc330c5b2d
Nolan Woods <nolan_w@sfu.ca>
2019-10-10 13:45:44 -0700

    Pull out brinkman services into seperate file

--- ui/src/IslandCompare/Navigation.vue
+++ ui/src/IslandCompare/Navigation.vue
@@ -18,17 +18,12 @@
 </template>
 
 <script>
+    import services from "@/brinkman_services";
     export default {
         name: "Navigation",
         data() {return{
             home: this.$router.options.routes.find(o=>o.path === "/"),
-            services: [
-                {name: "IslandViewer", path: "http://www.pathogenomics.sfu.ca/islandviewer/"},
-                {name: "PSORTb v.3.0", path: "https://www.psort.org/psortb/index.html"},
-                {name: "PSORTdb", path: "https://db.psort.org"},
-                {name: "Pseudomonas Genome Database", path: "http://www.pseudomonas.com"},
-                {name: "Burkholderia Genome Database", path: "http://www.burkholderia.com"},
-            ],
+            services: services,
         }},
         computed: {
             pages() {
---
+++ ui/src/brinkman_services.js
@@ -0,0 +1,7 @@
+export default [
+    {name: "IslandViewer", path: "http://www.pathogenomics.sfu.ca/islandviewer/"},
+    {name: "PSORTb v.3.0", path: "https://www.psort.org/psortb/index.html"},
+    {name: "PSORTdb", path: "https://db.psort.org"},
+    {name: "Pseudomonas Genome Database", path: "http://www.pseudomonas.com"},
+    {name: "Burkholderia Genome Database", path: "http://www.burkholderia.com"},
+];
