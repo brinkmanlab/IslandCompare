d383bd75d1583a97f18aa365e0aaaa59e273fef1
Nolan Woods <nolan_w@sfu.ca>
2019-10-24 10:53:48 -0700

    Add genomic islands download

--- ui/src/IslandCompare/JobHistory.vue
+++ ui/src/IslandCompare/JobHistory.vue
@@ -5,7 +5,8 @@
             <template v-slot:functions="slot">
                 <template v-if="slot.done">
                     <b-link v-bind:to="`/visualize/${slot.outputs['Results'].id}`">Visualize</b-link>
-                    <b-link v-bind:href="`/api/histories/${slot.model.history_id}/contents/${slot.outputs['Results'].id}/display?to_ext=gff3&filename=${encodeURIComponent(slot.model.history.name)}.gff3` | auth | galaxybase">Download</b-link>
+                    <b-link v-bind:href="`/api/histories/${slot.model.history_id}/contents/${slot.outputs['Results'].id}/display?to_ext=gff3&filename=${encodeURIComponent(slot.model.history.name)}.gff3` | auth | galaxybase">Results</b-link>
+                    <b-link v-bind:href="`/api/histories/${slot.model.history_id}/contents/${slot.outputs['Genomic Islands'].id}/display?to_ext=gff3&filename=${encodeURIComponent(slot.model.history.name)}.gff3` | auth | galaxybase">Genomic Islands</b-link>
                 </template>
             </template>
         </Jobs>
