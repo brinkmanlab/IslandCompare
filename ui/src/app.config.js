978179cf4587787490195bd539c799be3e90d170
Nolan Woods <nolan_w@sfu.ca>
2019-10-24 10:53:26 -0700

    Target 'galaxy' subdomain dynamically for galaxy_path

--- ui/src/app.config.js
+++ ui/src/app.config.js
@@ -5,8 +5,9 @@ Configuration options specific to the app
 export const workflow_name = "IslandCompare unpacked";
 export const base_path = '/';
 //export const galaxy_path = '/galaxy';
-//export const galaxy_path = `${window.location.protocol}//${window.location.hostname}:81`;
-export const galaxy_path = `${window.location.protocol}//galaxy.pathogenomics.sfu.ca`;
+export const galaxy_path = `${window.location.protocol}//${window.location.hostname.replace(/^islandcompare/, 'galaxy')}`;
+//export const galaxy_path = `${window.location.protocol}//galaxy.pathogenomics.sfu.ca`;
+//export const galaxy_path = `${window.location.protocol}//gateway.cedar.computecanada.ca:8459`;
 
 if (process.env.NODE_ENV === 'production') {
     // Production only
