6e95684f375b07589270519c9210d3e965ad194b
Nolan Woods <nolan_w@sfu.ca>
2019-10-24 16:22:17 -0700

    Add fixed galaxy_path for dev

--- ui/src/app.config.js
+++ ui/src/app.config.js
@@ -4,15 +4,14 @@ Configuration options specific to the app
 
 export const workflow_name = "IslandCompare unpacked";
 export const base_path = '/';
-//export const galaxy_path = '/galaxy';
-export const galaxy_path = `${window.location.protocol}//${window.location.hostname.replace(/^islandcompare/, 'galaxy')}`;
-//export const galaxy_path = `${window.location.protocol}//galaxy.pathogenomics.sfu.ca`;
-//export const galaxy_path = `${window.location.protocol}//gateway.cedar.computecanada.ca:8459`;
+export let galaxy_path;
 
 if (process.env.NODE_ENV === 'production') {
     // Production only
-
+    galaxy_path = `${window.location.protocol}//${window.location.hostname.replace(/^islandcompare/, 'galaxy')}`;
 } else {
     // Dev only
-
+    //export const galaxy_path = '/galaxy';
+    galaxy_path = `${window.location.protocol}//galaxy.pathogenomics.sfu.ca`;
+    //export const galaxy_path = `${window.location.protocol}//gateway.cedar.computecanada.ca:8459`;
 }
