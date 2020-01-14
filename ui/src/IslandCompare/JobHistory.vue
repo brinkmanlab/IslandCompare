4b4e2bf7cbf4c4682c73d3496860ba68a720ae6e
Nolan Woods <nolan@i2labs.ca>
2020-01-14 15:06:03 -0800

    Add newick to workflow output downloads

--- ui/src/IslandCompare/JobHistory.vue
+++ ui/src/IslandCompare/JobHistory.vue
@@ -3,10 +3,11 @@
         <h1>Job History</h1>
         <Jobs v-bind:invocationsPromise="invocationsPromise">
             <template v-slot:functions="slot">
-                <template v-if="slot.done && slot.outputs['Results']">
-                    <b-link v-bind:to="`/visualize/${slot.outputs['Results'].id}`">Visualize</b-link>
-                    <b-link v-bind:href="`/api/histories/${slot.model.history_id}/contents/${slot.outputs['Results'].id}/display?to_ext=gff3&filename=${encodeURIComponent(slot.model.history.name)}.gff3` | auth | galaxybase"><i class="icon-download"></i>Download</b-link>
-                    <b-link v-if="slot.outputs['Genomic Islands']" v-bind:href="`/api/histories/${slot.model.history_id}/contents/${slot.outputs['Genomic Islands'].id}/display?to_ext=gff3&filename=${encodeURIComponent(slot.model.history.name)}.gff3` | auth | galaxybase"><i class="icon-download"></i>Genomic Islands</b-link>
+                <template v-if="slot.done && slot.model.outputs['Results']">
+                    <b-link v-bind:to="`/visualize/${slot.model.outputs['Results'].id}`">Visualize</b-link>
+                    <b-link v-bind:href="`/api/histories/${slot.model.history_id}/contents/${slot.model.outputs['Results'].id}/display?to_ext=gff3&filename=${encodeURIComponent(slot.model.history.name)}.gff3` | auth | galaxybase"><i class="icon-download"></i>Download</b-link>
+                    <b-link v-if="slot.model.outputs['Genomic Islands']" v-bind:href="`/api/histories/${slot.model.history_id}/contents/${slot.model.outputs['Genomic Islands'].id}/display?to_ext=gff3&filename=${encodeURIComponent(slot.model.history.name)}.gff3` | auth | galaxybase"><i class="icon-download"></i>Genomic Islands</b-link>
+                    <b-link v-if="slot.model.outputs['Newick']" v-bind:href="`/api/histories/${slot.model.history_id}/contents/${slot.model.outputs['Newick'].id}/display?to_ext=newick&filename=${encodeURIComponent(slot.model.history.name)}.newick` | auth | galaxybase"><i class="icon-download"></i>Phylo Tree</b-link>
                 </template>
             </template>
         </Jobs>
