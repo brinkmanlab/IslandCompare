9a4785aa791c4cd7fb3cfbbb879400c70c23188d
Nolan Woods <nolan@i2labs.ca>
2020-01-14 15:00:58 -0800

    Remove Markdown.vue and tweak dev configs

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -8,10 +8,10 @@ module.exports = {
                 //pathRewrite: (path, req)=>path.replace('/galaxy', ''), //eslint-disable-line
                 //target: 'http://galaxy.brinkman.mbb.sfu.ca/',
                 //target: 'https://gateway.cedar.computecanada.ca:8457/',
-                target: 'http://ec2-13-57-236-209.us-west-1.compute.amazonaws.com/',
+                target: 'http://ec2-18-144-14-167.us-west-1.compute.amazonaws.com/',
                 changeOrigin: true,
                 ws: false,
             },
         }
     },
-}
+};
