4a0202d546fcdadf946b5eb4973fc7c9ca7a15ae
Nolan Woods <nolan@i2labs.ca>
2020-02-10 14:46:12 -0800

    Handle full domain when checking if hosting from expected subdomain

--- ui/src/app.config.js
+++ ui/src/app.config.js
@@ -11,10 +11,11 @@ export const base_path = publicPath;
 export let galaxy_path = `${window.location.protocol}//galaxy.islandcompare.ca`; // Default backend
 
 const subdomain = /^islandcompare/;
+const fulldomain = /^islandcompare.*\..*\..*$/;
 
 if (process.env.NODE_ENV === 'production') {
     // Production only
-    if (subdomain.test(window.location.hostname)) {
+    if (fulldomain.test(window.location.hostname)) {
         galaxy_path = `${window.location.protocol}//${window.location.hostname.replace(subdomain, 'galaxy')}`;
     } else {
         // In the event that the page is accessed via an unexpected subdomain, use default backend
