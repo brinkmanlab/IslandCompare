284ac4b2a73e65fe8e2ca0e6743b0607459b1053
Nolan Woods <nolan_w@sfu.ca>
2020-12-03 13:38:07 -0800

    Add destroy message

--- deployment/docker/destroy.sh
+++ deployment/docker/destroy.sh
@@ -5,3 +5,4 @@ terraform destroy -target=module.islandcompare -auto-approve
 terraform destroy -target=module.admin_user -auto-approve
 terraform destroy -target=module.galaxy -auto-approve
 terraform destroy -auto-approve
+echo 'If any errors occured during destroy (the likely did), you will need to delete your terraform.tfstate file before running ./deploy.sh again.'
