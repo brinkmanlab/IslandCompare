acec8915f8194608e851ef0fa744878d01be38f8
Nolan Woods <nolan_w@sfu.ca>
2020-12-11 12:15:45 -0800

    output galaxy admin api key

--- deployment/docker/outputs.tf
+++ deployment/docker/outputs.tf
@@ -3,6 +3,11 @@ output "galaxy_admin_password" {
   sensitive = true
 }
 
+output "galaxy_admin_api_key" {
+  value = module.admin_user.api_key
+  sensitive = true
+}
+
 output "galaxy_master_api_key" {
   value = module.galaxy.master_api_key
   sensitive = true
