4c0289ed66f69a7af53ee152585d0bf791064db3
Nolan Woods <nolan_w@sfu.ca>
2020-12-15 22:10:15 -0800

    Refactor workflow using optional inputs

--- destinations/galaxy/workflow.tf
+++ destinations/galaxy/workflow.tf
@@ -1,5 +1,5 @@
 locals {
-  workflow = file("${path.module}/../../workflow/workflows/IslandCompare_unpacked_w._BLAST.ga")
+  workflow = file("${path.module}/../../workflow/workflows/IslandCompare.ga")
 }
 
 resource "galaxy_stored_workflow" "islandcompare" {
