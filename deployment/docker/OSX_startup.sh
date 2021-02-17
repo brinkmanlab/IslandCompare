2b3606a0bdf8fb24e6c191d3e5a5a338b4a9a69d
Nolan Woods <nolan_w@sfu.ca>
2021-02-17 15:51:11 -0800

    Add link to osx_startup for OSX Docker workaround

--- deployment/docker/OSX_startup.sh
+++ deployment/docker/OSX_startup.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-docker run --rm --privileged --pid=host justincormack/nsenter1 -- mount --make-shared /tmp
+# https://github.com/docker/for-mac/issues/3431
+
+docker run --rm --privileged --pid=host justincormack/nsenter1 -- mount --verbose --make-shared /tmp
 terraform apply -target=module.islandcompare.docker_container.microbedb -auto-approve
