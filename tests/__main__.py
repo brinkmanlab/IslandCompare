4b82f0795fb61d9e4db8091a8d5f703c0c1f7b5e
Nolan Woods <nolan_w@sfu.ca>
2021-02-25 14:32:42 -0800

    Add workflow tests

--- tests/__main__.py
+++ tests/__main__.py
@@ -1,22 +1,7 @@
+#!/usr/bin/env python3
 import unittest
-import httpimport
 
-with httpimport.remote_repo("islandcompare", "https://raw.githubusercontent.com/brinkmanlab/islandcompare-cli/master/islandcompare.py"):
-    import islandcompare
-
-# Basic Pseudomonas
-
-# Basic Burkholeria
-
-# Draft without newick
-
-# Draft with newick
-
-# MCM Lockup
-# ERR388703.gbk -r NZ_LN999987.1
-
-# Empty BLAST failure
-# VC14135 and VC13213
+from . import *
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
