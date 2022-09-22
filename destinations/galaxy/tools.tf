d7175afdb078440ca730c49f7ed258a841e444f0
Nolan Woods <nolan_w@sfu.ca>
2022-09-22 12:57:14 -0700

    Ignore optional tool properties to avoid replacing them

--- destinations/galaxy/tools.tf
+++ destinations/galaxy/tools.tf
@@ -10,7 +10,7 @@ resource "galaxy_repository" "islandcompare" {
   changeset_revision = each.value.changeset_revision
 
   lifecycle {
-    ignore_changes = [changeset_revision]
+    ignore_changes = [changeset_revision,install_repository_dependencies,install_resolver_dependencies,install_tool_dependencies,remove_from_disk,sub_repositories,tools]
   }
 }
 
@@ -20,7 +20,7 @@ resource "galaxy_repository" "microbedb" {
   name = "microbedb"
   changeset_revision = "2f6ef3a184df"
   lifecycle {
-    ignore_changes = [changeset_revision]
+    ignore_changes = [changeset_revision,install_repository_dependencies,install_resolver_dependencies,install_tool_dependencies,remove_from_disk,sub_repositories,tools]
   }
 }
 
@@ -30,6 +30,6 @@ resource "galaxy_repository" "data_manager_fetch_genome_dbkeys_all_fasta" {
   name = "data_manager_fetch_genome_dbkeys_all_fasta"
   changeset_revision = "4d3eff1bc421"
   lifecycle {
-    ignore_changes = [changeset_revision]
+    ignore_changes = [changeset_revision,install_repository_dependencies,install_resolver_dependencies,install_tool_dependencies,remove_from_disk,sub_repositories,tools]
   }
 }
