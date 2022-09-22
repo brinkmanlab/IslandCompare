bedb89ec3e7edf79757417b6d8f342f7f907a6c1
Nolan Woods <nolan_w@sfu.ca>
2022-09-22 12:57:31 -0700

    Fix demo deployment cleanup

--- deployment/aws/destroy.sh
+++ deployment/aws/destroy.sh
@@ -4,4 +4,6 @@
 terraform destroy -target=module.islandcompare -auto-approve
 terraform destroy -target=module.admin_user -auto-approve
 terraform destroy -target=module.galaxy -auto-approve
+terraform destroy -target=kubernetes_persistent_volume_claim.microbedb -auto-approve
+terraform destroy -target=kubernetes_namespace.instance -auto-approve
 terraform destroy -auto-approve
